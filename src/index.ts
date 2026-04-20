2d2a83f3c083d3f44837dff4b006228e0f5a1b8e
andrewmurillas <andres.murillas.2025@mumail.ie>
2026-04-20 16:21:03 +0100

    oauth implemented

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express, {Application} from 'express';
 import {Db, MongoClient} from 'mongodb';
 import {connect} from './mongo-controller';
 import userRouter from './routes/user_routes';
+import authRouter from './routes/auth_routes';
+import './auth/passport';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/auth', authRouter);
 app.use('/', userRouter);
 
 const client: Promise<MongoClient> = connect();
